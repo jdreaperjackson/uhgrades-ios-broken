f2049e0b64b05929e81008c85a59adfef3ca313c
johnd.jackson <jdreaperjackson@gmail.com>
2021-09-19 20:52:24 -0500

    Initial Commit

---
+++ Shared/UHGradesApp.swift
@@ -0,0 +1,17 @@
+//
+//  UHGradesApp.swift
+//  Shared
+//
+//  Created by John D. Jackson on 9/19/21.
+//
+
+import SwiftUI
+
+@main
+struct UHGradesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
